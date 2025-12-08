9464f2bd6c64fe651d5a24ccd095d5e6298a0ba6
Dimitrios Simatos <dsimat@yahoo.com>
2025-12-08 12:45:22 -0500

    Added test parametrization

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,23 +1,37 @@
-import math
+import pytest
 
 from test_package.example import distance, kinetic_energy
 
 
-def test_distance() -> None:
+@pytest.mark.parametrize(
+    "p1, p2, result",
+    [
+        ((3, 0), (0, 4), 5.0),
+        ((1, 2), (4, 6), 5.0),
+        ((0, 0), (0, 0), 0.0),
+    ],
+)
+def test_distance(p1, p2, result) -> None:
     """Test the distance function"""
-    assert distance((3, 0), (0, 4)) == 5.0
-    assert distance((1, 2), (4, 6)) == 5.0
-    assert distance((0, 0), (0, 0)) == 0.0
-    assert distance((-1, -1), (1, 1)) == math.hypot(2, 2)
-    assert type(distance((1, 2), (4, 6))) is float
+    assert distance(p1, p2) == result
 
 
-def test_kinetic_energy() -> None:
+def test_distance_type() -> None:
+    """Test the distance type"""
+    assert type(distance((3, 0), (0, 4))) is float
+
+
+@pytest.mark.parametrize(
+    "mass, velocity, result",
+    [(2, 3, 9), (0, 10, 0), (5, 0, 0), (1.5, 4, 12)],
+)
+def test_kinetic_energy(mass, velocity, result) -> None:
     """Test the kinetic_energy function"""
-    assert kinetic_energy(2, 3) == 9.0
-    assert kinetic_energy(0, 10) == 0.0
-    assert kinetic_energy(5, 0) == 0.0
-    assert kinetic_energy(1.5, 4) == 12.0
+    assert kinetic_energy(mass, velocity) == result
+
+
+def test_kinetic_energy_type() -> None:
+    """Test the kinetic_energy type"""
     assert type(kinetic_energy(2, 3)) is float
 
 
