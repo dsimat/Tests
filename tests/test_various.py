69c7c5c7d06d9cd6d246fb1ecf16cb420d9225ed
Dimitrios Simatos <dsimat@yahoo.com>
2026-03-27 12:51:38 -0400

    Added comments and a test for exception handling

--- tests/test_various.py
+++ tests/test_various.py
@@ -1,6 +1,7 @@
 import pytest
 
 
+# Test for parameterization
 @pytest.mark.parametrize(
     "a,b,result",
     [
@@ -13,8 +14,15 @@ def test_add(a, b, result):
     assert a + b == result
 
 
+# Test for file operations
 def test_file(tmp_path):
     """Test file writing and reading"""
     file = tmp_path / "hello.txt"
     file.write_text("hi!")
     assert file.read_text() == "hi!"
+
+
+# Test for exception handling
+def test_zero_division():
+    with pytest.raises(ZeroDivisionError):
+        1 / 0
